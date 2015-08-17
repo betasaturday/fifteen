868ba628a28bcae86270edda6ba303789cf5a4bc
betasaturday <evgeniya.knm@gmail.com>
2015-08-17 07:48:37 +0300

    Add pause window

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 (function() {
     var field = [[], [], [], []], cellSize = 65, steps, time, timer,
-        cells = [];
+        cells = [], nums = [];
     var gameField = document.querySelector(".game-field"),
         shuffleBtn = document.querySelector(".shuffle-btn"),
         pauseBtn = document.querySelector(".pause-btn"),
-        quitBtn = document.querySelector(".quit-btn");
+        quitBtn = document.querySelector(".quit-btn"),
+        timeSpan = document.querySelector("span.time"),
+        stepsSpan = document.querySelector("span.steps"),
+        gameBoard = document.querySelector(".game-board"),
+        pauseWindow = document.querySelector(".pause-window"),
+        resumeBtn = document.querySelector(".pause-window > .icon"),
+        winWindow = document.querySelector(".win-window"),
+        gameWindow = document.querySelector(".game-window");
     
     var dx = [0, 1, 0, -1], dy = [-1, 0, 1, 0];
     
@@ -17,6 +24,23 @@
     });
     pauseBtn.addEventListener("click", pause);
     quitBtn.addEventListener("click", showScoreboard);
+    resumeBtn.addEventListener("click", function(e) {
+        var rect = resumeBtn.getBoundingClientRect(),
+            x = e.x - rect.left,
+            y = e.y - rect.top,
+            w = resumeBtn.clientWidth,
+            h = resumeBtn.clientHeight;
+        
+        var or1 = oriented(0, h, w, h/2, x, y),
+            or2 = oriented(0, 0, w, h/2, x, y);
+        
+        if (oriented(0, h, w, h/2, x, y) < 0
+            &&  oriented(0, 0, w, h/2, x, y) > 0)
+        {
+            resume();
+        }
+    });
+    
     for(var i = 1; i <= 15; ++i)
     {
         var cell = document.createElement("div");
@@ -26,6 +50,15 @@
         cell.innerHTML = i + "";
         gameField.appendChild(cell);
         cells.push(cell);
+    }
+    var numsLength = Math.floor(Math.random()*15) + 20;
+    for (var i = 0; i < numsLength; ++i)
+    {
+        var num = document.createElement("div"),
+            text = 1 + Math.floor(Math.random()*14) + "";
+        num.innerHTML = text;
+        nums.push(num);
+        pauseWindow.insertBefore(num, resumeBtn);
     }
     
     initialize();
@@ -54,6 +87,7 @@
         {
             field[Math.floor(i/4)][i%4] = (i + 1)%16;
         }
+        
     }
     
     function checkForMove(cell)
@@ -69,10 +103,16 @@
                 ++steps;
                 if (steps == 1)
                 {
-                    setInterval(timerTick, 1000);
+                    restartTimer();
                 }
                 requestAnimationFrame(function() {
                     move(cell, {y: i, x: j}, {y: ny, x: nx});
+                    if (win())
+                    {
+                        gameWindow.style.display = "none";
+                        winWindow.style.display = "block";
+                    }
+                    stepsSpan.innerHTML = steps;
                 });
                 break;
             }
@@ -102,6 +142,10 @@
     function shuffle()
     {
         initialize();
+        clearTimeout(timer);
+        drawTime();
+        stepsSpan.innerHTML = "0";
+        
         var empty = {x: 3, y: 3}, 
             moves = Math.floor(10 + Math.random()*90);
         while (moves--)
@@ -137,11 +181,91 @@
     }
     function drawTime()
     {
+        var m = Math.floor(time/60),
+            s = time%60;
+        
+        timeSpan.innerHTML = to2digits(m) + ":" + to2digits(s);
+    }
+    function to2digits(num)
+    {
+        var res = num + "";
+        if (res.length == 1)
+            res = "0" + res;
+        return res;
     }
     function pause()
     {
+        pauseTimer();
+        gameBoard.style.display = "none";
+        pauseWindow.style.display = "block";
+        var innerWidth = pauseWindow.clientWidth - 20,
+            innerHeight = pauseWindow.clientHeight - 20;
+        
+        for (var i = 0; i < nums.length; ++i)
+        {
+            var num = nums[i], fail = true;
+            while (fail)
+            {
+                num.style.fontSize = Math.floor(Math.random()*30)+16 + "px";
+                num.style.left = Math.floor(Math.random()*(innerWidth - num.clientWidth)) + "px";
+                num.style.top = Math.floor(Math.random()*(innerHeight - num.clientHeight)) + "px";       
+                if (noIntersections(i))
+                {
+                    fail = false;
+                }
+            }
+        }
     }
     function showScoreboard()
     {
+    }
+    function restartTimer()
+    {
+        timer = setInterval(timerTick, 1000);
+        time = 0;
+    }
+    function pauseTimer()
+    {
+        clearInterval(timer);
+    }
+    function resume()
+    {
+        if (steps > 0)
+        {
+            timer = setInterval(timerTick, 1000);
+        }
+        gameBoard.style.display = "block";
+        pauseWindow.style.display = "none";
+    }
+    function noIntersections(to)
+    {
+        for (var i = 0; i < to; ++i)
+        { 
+            if (isIntersection(nums[i].getBoundingClientRect(), nums[to].getBoundingClientRect()))
+                return false;
+        }
+        return true;
+    }
+    function isIntersection(a, b)
+    {
+        return (segmentsIntersect(a.left, a.right, b.left, b.right)
+            && segmentsIntersect(a.top, a.bottom, b.top, b.bottom));
+    }
+    function segmentsIntersect(a, b, c, d)
+    {
+        return !(c > b || a > d);
+    }
+    function oriented(ax, ay, bx, by, cx, cy)
+    {
+        return (bx - ax)*(cy - by) - (by - ay)*(cx - bx);
+    }
+    function win()
+    {
+        for (var i = 0; i < 16; ++i)
+        {
+            if (field[Math.floor(i/4)][i%4] != (i + 1)%16)
+                return false;
+        }
+        return true;
     }
 })();
